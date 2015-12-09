ed00d122cb7ac3aaf88c669b68aaae4cbb87aa22
Donald Hanson II <donaldhanson@mail.adelphi.edu>
2015-12-08 23:16:59 -0500

    UserTests updates

--- src/UsersTest.java
+++ src/UsersTest.java
@@ -27,4 +27,28 @@ public class UsersTest {
 		Users u = new Users();
 		assertEquals(u.getUsernameByID(1), "me");
 	}
+	
+	@Test
+	public void testcheckUsername() {
+		Users u = new Users();
+		assertEquals(u.checkUsername("dhansonii"), true);
+	}
+	
+	@Test
+	public void testcheckUsernameFalse() {
+		Users u = new Users();
+		assertEquals(u.checkUsername("john546"), false);
+	}
+	
+	@Test
+	public void getTypeByCredentials() {
+		Users u = new Users();
+		assertEquals(u.checkUserCredentials("hello", "goodbye"), true);
+	}
+	
+	@Test
+	public void getTypeByCredentialsFalse() {
+		Users u = new Users();
+		assertEquals(u.checkUserCredentials("Hello", "goodbye"), false);
+	}
 }
